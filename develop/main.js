a8ca55da248279005066a0c912fee6c9baa756f7
wilkdasilk <anwilk92@gmail.com>
2017-02-01 23:52:46 -0800

    alternate to move, reset on click

--- develop/main.js
+++ develop/main.js
@@ -22,34 +22,51 @@ var didWin = function(aPlayer) {
 
 
 $(document).ready(function(){
-
+  var need1= 83;
+  var need2=74;
   //when typing
   $('body').keydown(function moveRight(event){
     //as long as no one has won
     if(!(didWin(player1) || didWin(player2))){
-      //if s or d
-      if (event.which ===83 || event.which ===68){
+      //if needed key 83 then 68
+      if (event.which == need1){
+          //first change the status to race!
+        statusDiv.html("<p>Tap fast to win</p>");
         //move player one right 20px
           player1.animate({left: "+=20px"}, 0);
           //win?
           didWin(player1);
           //if win, reset button appears
           if(didWin(player1)){
-            resetDiv.html("<p>Play again</p>");
             resetDiv.css("visibility", "visible");
             }
+            //switch needed key
+            if (need1==83){
+              need1 = 68
+            }
+            else {
+              need1=83
+            };
           }
         //if j or k
-      if (event.which ===74 || event.which ===75){
+      if (event.which == need2){
+        //first change the status to race!
+        statusDiv.html("<p>Tap fast to win</p>");
          //move player two right 20px
          player2.animate({left: "+=20px"}, 0);
          //win?
          didWin(player2);
          //if win, reset button appears
          if(didWin(player2)){
-           resetDiv.html("<p>Play again</p>");
            resetDiv.css("visibility", "visible");
          }
+         //switch needed key
+         if (need2==74){
+           need2 = 75
+         }
+         else {
+           need2=74
+         };
        }
     }
 
@@ -57,6 +74,16 @@ $(document).ready(function(){
 
 
  })
+ //when click reset
+ resetDiv.on('click', function resetGame(){
+   //original style and classes
+   $('.player').attr("style", "left: 0px;");
+   statusDiv.html("<p>start!</p>");
+   resetDiv.css("visibility", "hidden");
+   need1 =83;
+   need2=74;
+ })
+
 });
 
   //keycode for s is 83
