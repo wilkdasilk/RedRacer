2757729ac2336170d3d00e68da1d5ee433e59172
wilkdasilk <anwilk92@gmail.com>
2017-02-02 00:20:14 -0800

    track score for multiple games

--- develop/main.js
+++ develop/main.js
@@ -22,8 +22,14 @@ var didWin = function(aPlayer) {
 
 
 $(document).ready(function(){
+  //initial keys to move
   var need1= 83;
   var need2=74;
+
+  //initial scores
+  var redScore =0;
+  var greenScore = 0;
+
   //when typing
   $('body').keydown(function moveRight(event){
     //as long as no one has won
@@ -34,19 +40,22 @@ $(document).ready(function(){
         statusDiv.html("<p>Tap fast to win</p>");
         //move player one right 20px
           player1.animate({left: "+=20px"}, 0);
+          //switch needed key
+          if (need1==83){
+            need1 = 68
+          }
+          else {
+            need1=83
+          };
           //win?
           didWin(player1);
           //if win, reset button appears
           if(didWin(player1)){
             resetDiv.css("visibility", "visible");
+            //update score
+            redScore++;
             }
-            //switch needed key
-            if (need1==83){
-              need1 = 68
-            }
-            else {
-              need1=83
-            };
+
           }
         //if j or k
       if (event.which == need2){
@@ -54,12 +63,6 @@ $(document).ready(function(){
         statusDiv.html("<p>Tap fast to win</p>");
          //move player two right 20px
          player2.animate({left: "+=20px"}, 0);
-         //win?
-         didWin(player2);
-         //if win, reset button appears
-         if(didWin(player2)){
-           resetDiv.css("visibility", "visible");
-         }
          //switch needed key
          if (need2==74){
            need2 = 75
@@ -67,11 +70,17 @@ $(document).ready(function(){
          else {
            need2=74
          };
+         //win?
+         didWin(player2);
+         //if win, reset button appears
+         if(didWin(player2)){
+           resetDiv.css("visibility", "visible");
+           //update score
+           greenScore++;
+
+          }
+        }
        }
-    }
-
-
-
 
  })
  //when click reset
@@ -80,13 +89,14 @@ $(document).ready(function(){
    $('.player').attr("style", "left: 0px;");
    statusDiv.html("<p>start!</p>");
    resetDiv.css("visibility", "hidden");
+   //show new score
+   $(".score").css('visibility', "visible");
+   $('.score.green').text(greenScore);
+   $('.score.red').text(redScore);
+
+
    need1 =83;
    need2=74;
  })
 
 });
-
-  //keycode for s is 83
-  //keycode for d is 68
-  //keycode for j is 74
-  //keycode for k is 75
